489825749265f49b3b1ee3940a98e9e1aecbe8a7
Alizarix <85684889+Alizarix@users.noreply.github.com>
2021-11-07 21:48:35 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <set>
+#include <algorithm>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	string line;
+	getline(cin, line);
+	line = regex_replace(line, regex(" +"), ""); // убираем пробелы
+	unsigned k; //положительное целое
+	cin >> k;
+	auto length = line.length();
+	if (k >= length)
+		cout << line;
+	else {
+		set <string> st;  //контейнер set
+		auto comb = string(line.begin(), line.begin() + k);
+		st.insert(comb);
+		while (next_permutation(line.begin(), line.end())) {
+			comb = string(line.begin(), line.begin() + k);
+			if (is_sorted(comb.begin(), comb.end())) st.insert(comb);
+		}
+		auto width = k + 1;
+		for (auto& token : st) cout << token << ' ';
+		cout.put('\n');
+
+	}
+
+	system("pause");
+
+}
